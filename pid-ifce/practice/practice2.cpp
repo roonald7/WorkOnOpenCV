d11fd2c140df7c44e60a3c42002ddcb5d8c3dff7
roonald7 <roonald.anderson017@gmail.com>
2022-09-03 10:18:42 -0300

    ## implemented practice 3

--- pid-ifce/practice/practice2.cpp
+++ pid-ifce/practice/practice2.cpp
@@ -13,15 +13,18 @@ int main(int, char**)
     auto windowLabel2 = "Display Resized Image (Nearest Interpolation) - Practice 2";
     auto windowLabel3 = "Display Resized Image (Bilinear Interpolation) - Practice 2";
     auto windowLabel4 = "Display Resized Image (Bicubic Interpolation) - Practice 2";
+    auto windowLabel5 = "Display Resized Image (Bicubic Interpolation 2x) - Practice 2";
 
     auto image = imread(imagePath);
     Mat imgResizeNearest;
     Mat imgResizeBilinear;
     Mat imgResizeBicubic;
+    Mat imgResizeBicubic2x;
 
     resize(image, imgResizeNearest, Size(), 3.33, 3.33, INTER_NEAREST);
     resize(image, imgResizeBilinear, Size(), 3.33, 3.33, INTER_LINEAR);
     resize(image, imgResizeBicubic, Size(), 3.33, 3.33, INTER_CUBIC);
+    resize(image, imgResizeBicubic2x, Size(), 2, 2, INTER_CUBIC);
 
     namedWindow(windowLabel1, WINDOW_AUTOSIZE);
     imshow(windowLabel1, image);
@@ -34,6 +37,9 @@ int main(int, char**)
 
     namedWindow(windowLabel4, WINDOW_AUTOSIZE);
     imshow(windowLabel4, imgResizeBicubic);
+    
+    namedWindow(windowLabel5, WINDOW_AUTOSIZE);
+    imshow(windowLabel5, imgResizeBicubic2x);
 
     waitKey(0);
 }
